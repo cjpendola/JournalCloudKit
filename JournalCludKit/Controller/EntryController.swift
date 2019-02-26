afa6825a557fa3c88c98894349186b2801f78d03
Carlos Pendola <cjpendola@hotmail.com>
2019-02-25 23:52:09 -0700

    Journal CloudKit

---
+++ JournalCludKit/Controller/EntryController.swift
@@ -0,0 +1,9 @@
+//
+//  EntryController.swift
+//  JournalCludKit
+//
+//  Created by Carlos Pendola on 2/25/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+import Foundation
---
+++ JournalCludKit/Model/Entry.swift
@@ -0,0 +1,9 @@
+//
+//  Entry.swift
+//  JournalCludKit
+//
+//  Created by Carlos Pendola on 2/25/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+import Foundation
